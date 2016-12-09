635fb01f176afb0443a784f3d2b4211d674142b8
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-12-09 13:46:21 -0600

    Updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
-// for `linter-eslint` Atom package, not grunt
 'use strict';
 
-var Config = require('@esscorp/eslint').configs.backend;
 
-delete Config.rules['lowercase-require']; // ok because grunt does not run this file
-
-
-module.exports = Config;
+module.exports = require('@esscorp/eslint').configs.backend;
